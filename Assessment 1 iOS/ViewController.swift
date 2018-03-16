1be649c066e529666d6a908ed0c89bda834bccec
BSASEF01 <bersabehsolomon16@gmail.com>
2018-03-16 14:35:57 -0400

    assessment 1 ios

--- Assessment 1 iOS/ViewController.swift
+++ Assessment 1 iOS/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var UILabel: UILabel!
+    
+    @IBOutlet weak var UITextField: UITextField!
+    
+    var textInput  = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        UITextField.text = textInput 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
-
+    @IBAction func UIButton(_ sender: Any) {
+       UILabel.text = UITextField.text
+    
+    }
+    
 }
 
